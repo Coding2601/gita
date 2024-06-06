2bc601c30444fed6372d95ed058c9487175a304c
shivam <shivamkmishra9@gmail.com>
2024-06-06 21:30:51 +0530

    new-update-2

--- manager/views.py
+++ manager/views.py
@@ -22,7 +22,9 @@ def update_add_bookmark(request):
             print(body)
             #username = 'shivamkmishra9'
             response = add_bookmark(body['data']['username'], body['data']['data'])
-            return JsonResponse({'message': response}, status=200)
+            res = JsonResponse({'message': response}, status=200)
+            res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+            return res
         except Exception as e:
             print(e)
             return None
@@ -35,7 +37,9 @@ def update_add_favourite(request):
         body = get_request_body(request)
         print(body)
         response = add_favourite(body['data']['username'], body['data']['data'])
-        return JsonResponse({'message': response}, status=200)
+        res = JsonResponse({'message': response}, status=200)
+        res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+        return res
     
 @csrf_exempt
 def update_remove_bookmark(request):
@@ -46,7 +50,9 @@ def update_remove_bookmark(request):
         print(body)
         response = remove_bookmark(body['data']['username'], body['data']['data'])
         print(response)
-        return JsonResponse({'message': response}, status=200)
+        res = JsonResponse({'message': response}, status=200)
+        res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+        return res
 
 @csrf_exempt
 def update_remove_favourite(request):
@@ -56,15 +62,21 @@ def update_remove_favourite(request):
         body = get_request_body(request)
         print(body)
         response = remove_favourite(body['data']['username'], body['data']['data'])
-        return JsonResponse({'message': response}, status=200)
+        res = JsonResponse({'message': response}, status=200)
+        res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+        return res
     
 def update_get_favourite(request, username):
     #username = 'shivamkmishra9'
     response = get_favourites(username)
-    return JsonResponse(response, status=200)
+    res = JsonResponse(response, status=200)
+    res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+    return res
 
 def update_get_bookmark(request, username):
     #username = 'shivamkmishra9'
     response = get_bookmarks(username)
     print(response)
-    return JsonResponse(response, status=200)        
+    res = JsonResponse(response, status=200)        
+    res['Access-Control-Allow-Origin'] = 'https://bhaagvad-gita.netlify.app'
+    return res
