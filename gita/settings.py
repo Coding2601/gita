3b7698fff0495a942ed2843a26e9311612802ed3
shivam <shivamkmishra9@gmail.com>
2024-06-06 15:05:42 +0530

    disabled_cors-2

--- gita/settings.py
+++ gita/settings.py
@@ -75,6 +75,13 @@ TEMPLATES = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_REPLACE_HTTPS_REFERER = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://bhagavad-gita.netlify.app'
+]
+CORS_ALLOW_HEADERS = [ "accept", "referer", "accept-encoding", "authorization", "content-type", "dnt", "origin", "user-agent", "x-csrftoken", "x-sessionid", "x-requested-with"]
+
 
 WSGI_APPLICATION = 'gita.wsgi.application'
 
