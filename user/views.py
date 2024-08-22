e955a11985c079090c4b82c1dba8d9f215dc8232
Shivam Mishra <shivamkmishra9@gmail.com>
2024-08-22 14:20:58 +0530

    Set Cookie Issue-4

--- user/views.py
+++ user/views.py
@@ -111,7 +111,7 @@ def login(request):
 
         token = jwt.encode(payload, 'secret', algorithm='HS256')
         response = JsonResponse({'message': 'success'}, status=200)
-        response.set_cookie(key='jwt', value=token, httponly=True, secure=True, samesite='Strict')
+        response.set_cookie(key='jwt', value=token, httponly=True, secure=True, samesite='None')
         print(token)
         return response
 
