8fb8e6ce189e890ba4a0daba0d5d30a5f67e2431
shivam <shivamkmishra9@gmail.com>
2026-02-09 20:41:15 +0530

    removed dotenv code

--- gita/settings.py
+++ gita/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
 
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
